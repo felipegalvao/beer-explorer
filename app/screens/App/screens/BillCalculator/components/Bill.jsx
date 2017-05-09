b0a3878401defeb961e61ae68b2a76787a9abd88
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-08 22:37:40 -0300

    Move connect from Bill to the BillCalculator component

--- app/screens/App/screens/BillCalculator/components/Bill.jsx
+++ app/screens/App/screens/BillCalculator/components/Bill.jsx
@@ -4,40 +4,35 @@ import BillTotal from "./BillTotal.jsx";
 
 import { connect } from "react-redux";
 
-export class Bill extends React.Component {
-  render() {
-    const { bill } = this.props;
-    const billItems:ArrayOf<Object> = bill.billItems;
-    const includeTip:boolean = bill.includeTip;
-    const tipPercentage:number = bill.tipPercentage;
-    const personsToPay:number = bill.personsToPay;
-    let billValue = 0;
+export const Bill = props => {
+  const billItems: ArrayOf<Object> = props.billItems;
+  const includeTip: boolean = props.includeTip;
+  const tipPercentage: number = props.tipPercentage;
+  const personsToPay: number = props.personsToPay;
+  let billValue = 0;
 
-    // Calculate total value of the bill
-    for (let i = 0; i < billItems.length; i++) {
-      billValue += billItems[i].unitPrice * billItems[i].quantity;
-    }
-
-    if (includeTip) {
-      billValue = billValue + (billValue * tipPercentage / 100);
-    }
-
-    const valuePerPerson = billValue / personsToPay;
-
-    return (
-      <div className="box">
-        <h2 className="title is-2 has-text-centered">Add Bill Item</h2>
-        {billItems.length > 0
-          ? ""
-          : <p>Your Bill does not have any items</p>}
-        <BillItemList billItems={billItems} />
-        {billItems.length > 0
-          ? <BillTotal billValue={billValue} valuePerPerson={valuePerPerson} />
-          : ""}
-      </div>
-    );
+  // Calculate total value of the bill
+  for (let i = 0; i < billItems.length; i++) {
+    billValue += billItems[i].unitPrice * billItems[i].quantity;
   }
-}
+
+  if (includeTip) {
+    billValue = billValue + billValue * tipPercentage / 100;
+  }
+
+  const valuePerPerson = billValue / personsToPay;
+
+  return (
+    <div className="box">
+      <h2 className="title is-2 has-text-centered">Add Bill Item</h2>
+      {billItems.length > 0 ? "" : <p>Your Bill does not have any items</p>}
+      <BillItemList billItems={billItems} />
+      {billItems.length > 0
+        ? <BillTotal billValue={billValue} valuePerPerson={valuePerPerson} />
+        : ""}
+    </div>
+  );
+};
 
 export default connect(state => {
   return state;
--- app/screens/App/screens/BillCalculator/index.js
+++ app/screens/App/screens/BillCalculator/index.js
@@ -2,8 +2,16 @@ import React, { Component } from "react";
 import Bill from "./components/Bill.jsx";
 import AddBillItem from "./components/AddBillItem.jsx";
 import BillOptions from "./components/BillOptions.jsx";
+import { connect } from "react-redux";
 
-const BillCalculator = () => {
+const BillCalculator = ({ bill }) => {
+  const billItems: ArrayOf<Object> = bill.billItems;
+  console.log(billItems);
+  const includeTip: boolean = bill.includeTip;
+  const tipPercentage: number = bill.tipPercentage;
+  const personsToPay: number = bill.personsToPay;
+  let billValue = 0;
+
   return (
     <section className="section">
       <div className="container">
@@ -14,7 +22,12 @@ const BillCalculator = () => {
             <BillOptions />
           </div>
           <div className="column is-half">
-            <Bill />
+            <Bill
+              billItems={billItems}
+              includeTip={includeTip}
+              tipPercentage={tipPercentage}
+              personsToPay={personsToPay}
+            />
           </div>
         </div>
       </div>
@@ -22,4 +35,10 @@ const BillCalculator = () => {
   );
 };
 
-export default BillCalculator;
+// function mapStateToProps(state) {
+//   return { bill: state.bill };
+// };
+
+export default connect(state => {
+  return state;
+})(BillCalculator);
